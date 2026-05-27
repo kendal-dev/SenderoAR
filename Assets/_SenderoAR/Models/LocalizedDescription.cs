5bbfeecd0f57a1168902c93ae414292224c27fa4
kendal-dev <lozano.daniel.dev@gmail.com>
2026-05-26 20:24:57 -0400

    feat(data): initialize 5 monument data assets with baseline metadata
    mon_01_templo, mon_02_capilla, mon_03_torre, mon_04_boveda, mon_05_pila.
    
    Baseline: ID, foundation year, GPS, anchor key placeholder, physical size 0.60x0.60 m (MDF base final), confidence 0.4, IsPhysicalInDefense per Camino A (1,3,5).
    
    Prefab + narration + description deferred to Sprints 4/6/7.

--- Assets/_SenderoAR/Models/LocalizedDescription.cs
+++ Assets/_SenderoAR/Models/LocalizedDescription.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class LocalizedDescription : MonoBehaviour
+namespace SenderoAR.Models
 {
-    // Start is called before the first frame update
-    void Start()
+    [Serializable]
+    public class LocalizedDescription
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [TextArea(3, 8)] public string Es;
+        [TextArea(3, 8)] public string En;
+        [TextArea(3, 8)] public string Pt;
     }
 }
